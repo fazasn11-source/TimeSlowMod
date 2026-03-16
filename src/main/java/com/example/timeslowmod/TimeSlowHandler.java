c5ee5e21ebd327e210e07db7ca5833dfee03f82a
fazasn11-source <fazasn11@gmail.com>
2026-03-16 18:07:51 +0500

    Update TimeSlowHandler.java

--- src/main/java/com/example/timeslowmod/TimeSlowHandler.java
+++ src/main/java/com/example/timeslowmod/TimeSlowHandler.java
@@ -1,30 +1,85 @@
 package com.example.timeslowmod;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.eventbus.api.IEventBus;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import java.util.Iterator;
 
-@Mod(TimeSlowMod.MOD_ID)
-public class TimeSlowMod {
-    public static final String MOD_ID = "timeslowmod";
-    public static final Logger LOGGER = LogManager.getLogger();
+public class TimeSlowHandler {
+    private float smoothTransition = 0f;
+    private final float TRANSITION_SPEED = 0.03f;
+    private int cooldown = 0;
     
-    public static boolean isTimeSlowed = false;
+    private long lastGameTime = 0;
+    private float visualTimeScale = 1.0f;
 
-    public TimeSlowMod() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::clientSetup);
-        MinecraftForge.EVENT_BUS.register(this);
+    @SubscribeEvent
+    public void onKeyInput(InputEvent.KeyInputEvent event) {
+        Minecraft mc = Minecraft.getInstance();
+        
+        if (ModKeybinds.TIME_SLOW_KEY.isDown() && mc.player != null && cooldown <= 0) {
+            // Проверяем что клавиша была нажата в этом тике
+            if (cooldown == 0) {
+                TimeSlowMod.isTimeSlowed = !TimeSlowMod.isTimeSlowed;
+                
+                if (TimeSlowMod.isTimeSlowed) {
+                    mc.player.displayClientMessage(
+                        new StringTextComponent(TextFormatting.AQUA + "⏰ Время замедлено!"), 
+                        true
+                    );
+                    
+                    if (mc.level != null) {
+                        mc.level.playSound(mc.player, mc.player.blockPosition(),
+                            SoundEvents.BUBBLE_COLUMN_WHIRLPOOL_AMBIENT, 
+                            SoundCategory.PLAYERS, 0.5f, 0.3f);
+                    }
+                } else {
+                    mc.player.displayClientMessage(
+                        new StringTextComponent(TextFormatting.GRAY + "⏰ Обычное время"), 
+                        true
+                    );
+                    
+                    if (mc.level != null) {
+                        mc.level.playSound(mc.player, mc.player.blockPosition(),
+                            SoundEvents.BUBBLE_COLUMN_UPWARDS_AMBIENT, 
+                            SoundCategory.PLAYERS, 0.3f, 0.7f);
+                    }
+                }
+                
+                cooldown = 5;
+            }
+        }
     }
 
-    private void clientSetup(final FMLClientSetupEvent event) {
-        ModKeybinds.register();
-        MinecraftForge.EVENT_BUS.register(new TimeSlowHandler());
-        MinecraftForge.EVENT_BUS.register(new TimeOverlay());
-        LOGGER.info("Time Slow Mod loaded!");
+    @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            Minecraft mc = Minecraft.getInstance();
+            
+            if (mc.level != null && mc.player != null) {
+                if (cooldown > 0) cooldown--;
+                
+                // Плавный переход
+                if (TimeSlowMod.isTimeSlowed && smoothTransition < 0.5f) {
+                    smoothTransition = smoothTransition + TRANSITION_SPEED;
+                    if (smoothTransition > 0.5f) smoothTransition = 0.5f;
+                } else if (!TimeSlowMod.isTimeSlowed && smoothTransition > 0) {
+                    smoothTransition = smoothTransition - TRANSITION_SPEED;
+                    if (smoothTransition < 0) smoothTransition = 0;
+                }
+                
+                // В Minecraft 1.16.5 мы не можем напрямую замедлить время,
+                // но можем создать визуальный эффект через управление рендером
+                visualTimeScale = 1.0f - smoothTransition;
+                
+                // Здесь можно добавить другие визуальные эффекты
+            }
+        }
     }
 }
