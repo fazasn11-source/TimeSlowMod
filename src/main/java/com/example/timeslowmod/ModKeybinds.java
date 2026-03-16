edca0f0064076962e8effc78ed41e83bf32a2392
fazasn11-source <fazasn11@gmail.com>
2026-03-16 17:36:14 +0500

    Update ModKeybinds.java

--- src/main/java/com/example/timeslowmod/ModKeybinds.java
+++ src/main/java/com/example/timeslowmod/ModKeybinds.java
@@ -1,1 +1,19 @@
+package com.example.timeslowmod;
 
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import org.lwjgl.glfw.GLFW;
+
+public class ModKeybinds {
+    public static KeyBinding TIME_SLOW_KEY;
+
+    public static void register() {
+        TIME_SLOW_KEY = new KeyBinding(
+            "key.timeslowmod.slow", 
+            GLFW.GLFW_KEY_B, 
+            "key.categories.timeslowmod"
+        );
+        
+        ClientRegistry.registerKeyBinding(TIME_SLOW_KEY);
+    }
+}
